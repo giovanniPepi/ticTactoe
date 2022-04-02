613ba9b327a33106a3959d6725b323178a5dbe2a
gtpepi <gtpepi@hotmail.com>
2022-04-02 18:24:50 -0300

    Wrap functions around a main

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
-// handles DOM manipulation
-const dQuery = (function(){
-    const gameboardContainer = document.querySelector(".gameboardContainer");
-    
-    getGrid = () => {
-        // sets CSS style for each square
-        styleSetter = (gameUnit) => {
-            gameUnit.setAttribute("class", "gameUnit");
-            gameUnit.textContent="fuck";
-            gameboardContainer.appendChild(gameUnit);
-        };
-        // loops to create the game board;                
-        gridCreator = (a) => {
-            for (let i = 0; i < 3; i++){
-                let gameUnit = document.createElement("div");
-                gameUnit.style.gridArea= `${a}/${i+1}`;
-                styleSetter(gameUnit);
+main = (() => {
+    // handles DOM manipulation
+    dQuery = (function(){
+        const gameboardContainer = document.querySelector(".gameboardContainer");
+        getGrid = () => {
+            // sets CSS style for each square
+            styleSetter = (gameUnit) => {
+                gameUnit.setAttribute("class", "gameUnit");
+                gameUnit.textContent="fuck";
+                gameboardContainer.appendChild(gameUnit);
             };
-        }
-        //calls gridcreator 3x
-        for (let i=0; i<3; i++){
-            gridCreator(i+1);
+            // loops to create the game board;                
+            gridCreator = (a) => {
+                for (let i = 0; i < 3; i++){
+                    let gameUnit = document.createElement("div");
+                    gameUnit.style.gridArea= `${a}/${i+1}`;
+                    styleSetter(gameUnit);
+                };
+            }
+            //calls gridcreator 3x
+            for (let i=0; i<3; i++){
+                gridCreator(i+1);
+            };
         };
-    };
-
+    return {
+        gameboardContainer,
+    }
 })();
 
-getGrid();
+
+
+//invocation
+getGrid(); 
+})();
