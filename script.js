36788c9b4b52229caae1bba75fcfd1bbd7491113
gtpepi <gtpepi@hotmail.com>
2022-04-03 19:09:48 -0300

    Add check function to write only once to grid

--- script.js
+++ script.js
@@ -13,31 +13,35 @@
                 element.setAttribute("id", "gameUnitO");
             }
         };
-
         return {
             gameboard, gameUnitContainer, gameUnit,
         }
     // imediatelly calls it before listeners
     })();
-
     const getListeners = (() => {
-        /*Listens for clicks, pass ID and calls write 
-        function for the element clicked */
+        /*Listens for clicks on parent node, pass ID of child and calls write 
+        function */
         dQuery.gameUnitContainer.forEach((unit) => 
             unit.addEventListener("click", () => {
-                unit = unit.firstChild;
-                writeUnit(unit, game.currentPlay);
+                // exits if already played;
+                if (unit.textContent !== '') return;
+                gameUnit = unit.firstChild;
+                currentPlay = changeTurn();
+                writeUnit(gameUnit, currentPlay);
             })
         );   
     })();
-
     //defines game logic
     const game = (() => {
         // will be changed each round
+
         let currentPlay = 'O';
+        changeTurn = () => {
+            return currentPlay === 'X'? currentPlay = 'O' : currentPlay = 'X';
+        }
 
         return {
-            currentPlay,
+            changeTurn,
         }
     })();
 
