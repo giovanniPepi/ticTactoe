08b5dd673b80717c2f8e6692b1a94899f8769ca2
gtpepi <gtpepi@hotmail.com>
2022-04-03 18:39:06 -0300

    Add CSS animations for plays

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
     // handles DOM manipulation
     dQuery = (function(){
         const gameboard = document.querySelector(".gameboard");
+        const gameUnitContainer = document.querySelectorAll(".gameUnitContainer");
         const gameUnit = document.querySelectorAll(".gameUnit");
 
         // writes plays according to their info
         writeUnit = (element, arg) => {
             element.textContent = arg;
             if (arg === 'X') {
-                element.setAttribute("class", "gameUnitX");
+                element.setAttribute("id", "gameUnitX");
             } else if (arg === 'O') {
-                element.setAttribute("class", "gameUnitO");
+                element.setAttribute("id", "gameUnitO");
             }
         };
 
         return {
-            gameboard, gameUnit,
+            gameboard, gameUnitContainer, gameUnit,
         }
     // imediatelly calls it before listeners
     })();
@@ -22,9 +23,9 @@
     const getListeners = (() => {
         /*Listens for clicks, pass ID and calls write 
         function for the element clicked */
-        dQuery.gameUnit.forEach((unit) => 
+        dQuery.gameUnitContainer.forEach((unit) => 
             unit.addEventListener("click", () => {
-                console.log(unit.id);
+                unit = unit.firstChild;
                 writeUnit(unit, game.currentPlay);
             })
         );   
