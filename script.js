a5acccd1a9b3133673a8dc0fcdc4213fb2f3e7cb
gtpepi <gtpepi@hotmail.com>
2022-04-02 23:28:58 -0300

    Change getgrid logic, add gameUnit nodelist

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ main = (() => {
     // handles DOM manipulation
     dQuery = (function(){
         const gameboard = document.querySelector(".gameboard");
+        const gameUnit = document.querySelectorAll(".gameUnit");
+
+        writeUnit = (element, arg) => {
+            element.textContent=arg;
+        }
         getGrid = () => {
-            // sets CSS style for each square
-            styleSetter = (gameUnit) => {
-                gameUnit.setAttribute("class", "gameUnit");
-                gameUnit.textContent="";
-                gameboard.appendChild(gameUnit);
-            };
             // loops to create the game board;                
             gridCreator = (a) => {
                 for (let i = 0; i < 3; i++){
@@ -17,18 +16,24 @@ main = (() => {
                     styleSetter(gameUnit);
                 };
             }
+            // sets CSS style for each square
+            styleSetter = (gameUnit) => {
+                gameUnit.setAttribute("class", "gameUnit");
+                writeUnit(gameUnit, '');
+                gameboard.appendChild(gameUnit);
+            };
+            
             //calls gridcreator 3x
             for (let i=0; i<3; i++){
                 gridCreator(i+1);
             };
         };
     return {
-        gameboard,
+        gameboard, gameUnit, writeUnit,
     }
 })();
 
 
-
-//invocation
+//activating
 getGrid(); 
 })();
