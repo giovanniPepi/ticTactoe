1454d6906c5fe3b4167bf04f289019bc095563cd
gtpepi <gtpepi@hotmail.com>
2022-04-03 11:21:00 -0300

    Move gameUnit creation outside of JS to the HTML file

--- script.js
+++ script.js
@@ -5,30 +5,9 @@
         writeUnit = (element, arg) => {
             element.textContent = arg;
         };
-        getGrid = (function() {
-            // loops to create the game board;                
-            gridCreator = (a) => {
-                for (let i = 0; i < 3; i++){
-                    let gameUnit = document.createElement("div");
-                    gameUnit.style.gridArea= `${a}/${i+1}`;
-                    styleSetter(gameUnit);
-                };
-            }
-            // sets CSS style for each square
-            styleSetter = (gameUnit) => {
-                gameUnit.setAttribute("class", "gameUnit");
-                writeUnit(gameUnit, 'fuck');
-                gameboard.appendChild(gameUnit);
-            };
-            
-            //calls gridcreator 3x
-            for (let i=0; i<3; i++){
-                gridCreator(i+1);
-            };
-        })();
 
         return {
-            gameboard, writeUnit,
+            gameboard, 
         }
     // imediatelly calls it before listeners
     })();
@@ -48,3 +27,5 @@
 
 
 //activating
+
+
