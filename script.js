b9af22d834adf484a4b2fbc6d90ba73459ce7182
gtpepi <gtpepi@hotmail.com>
2022-04-03 22:22:11 -0300

    Update entire gameUnit logic, add game control to its own fucntion

--- script.js
+++ script.js
@@ -1,53 +1,47 @@
-main = (() => {
-    
+/* main = (() => {
+ */    
 // handles DOM manipulation
     const dQuery = (function(){
         const gameboard = document.querySelector(".gameboard");
         const gameUnitContainer = document.querySelectorAll(".gameUnitContainer");
         const gameUnit = document.querySelectorAll(".gameUnit");
 
-        // writes plays according to their info
-        const writeUnit = (element, arg) => {
-            element.textContent = arg;
-            if (arg === 'X') {
-                element.setAttribute("id", "gameUnitX");
-            } else if (arg === 'O') {
-                element.setAttribute("id", "gameUnitO");
-            }
-        };
         return {
-            gameboard, gameUnitContainer, gameUnit, writeUnit, 
         }
-    })();
-//handles eventListeners
-    const getListeners = (() => {
-        /*Listens for clicks on parent node, pass ID of child and calls write 
-        function */
-        dQuery.gameUnitContainer.forEach((unit) => 
-            unit.addEventListener("click", () => {
-                // exits if already played;
-                if (unit.textContent !== '') return;
-                gameUnit = unit.firstChild;
-                currentPlay = game.changeTurn();
-                dQuery.writeUnit(gameUnit, currentPlay);
-            })
-        );   
     })();
 
 //handles game logic
     const game = (() => {
-        // will be changed each round
-        let _currentPlay = 'O';
-        const changeTurn = () => {
-            return _currentPlay === 'X'? _currentPlay = 'O' : _currentPlay = 'X';
+        const _gameboard = new Array(9);
+
+        const setUnit = (position, sign) =>{
+            _gameboard[position] = sign;
+        };
+
+        const getUnit = (position) => {
+            console.log({_gameboard});
+            return _gameboard[position];
         }
+
+        const resetBoard = () => {
+            for (i = 0; i < _gameboard.length; i++) {
+                _gameboard[i] = '';
+            }
+        };
+
+
+
         return {
-            changeTurn,     
+            setUnit, 
+            getUnit,
+            resetBoard,
         }
     })();
 
+// function that holds player info;
 
 
-})();
 
+/* })();
+ */
 
