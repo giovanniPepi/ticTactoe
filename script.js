8aba14c4e91a3ce0e0301f69fd5d3b769f0f6d4a
gtpepi <gtpepi@hotmail.com>
2022-04-03 22:33:50 -0300

    Update code readability

--- script.js
+++ script.js
@@ -1,47 +1,46 @@
-/* main = (() => {
- */    
 // handles DOM manipulation
-    const dQuery = (function(){
-        const gameboard = document.querySelector(".gameboard");
-        const gameUnitContainer = document.querySelectorAll(".gameUnitContainer");
-        const gameUnit = document.querySelectorAll(".gameUnit");
-
-        return {
-        }
-    })();
+const dQuery = (function(){
+  const gameUnitContainer = document.querySelectorAll(".gameUnitContainer");
+  const gameUnit = document.querySelectorAll(".gameUnit");
+  
+  return {
+    gameboard, gameUnitContainer, gameUnit,
+  }
+})();
 
 //handles game logic
-    const game = (() => {
-        const _gameboard = new Array(9);
+const game = (() => {
+  const _gameboard = new Array(9);
+  const setUnit = (position, sign) =>{
+    _gameboard[position] = sign;
+  };
 
-        const setUnit = (position, sign) =>{
-            _gameboard[position] = sign;
-        };
+  const getUnit = (position) => {
+    return _gameboard[position];
+  }
+  const resetBoard = () => {
+    for (i = 0; i < _gameboard.length; i++) {
+      _gameboard[i] = '';
+    }
+  };
 
-        const getUnit = (position) => {
-            console.log({_gameboard});
-            return _gameboard[position];
-        }
+  return {
+    setUnit, 
+    getUnit,
+    resetBoard,
+    }
+})();
 
-        const resetBoard = () => {
-            for (i = 0; i < _gameboard.length; i++) {
-                _gameboard[i] = '';
-            }
-        };
+const Player = () => {
+
+  
+}
 
 
 
-        return {
-            setUnit, 
-            getUnit,
-            resetBoard,
-        }
-    })();
+
 
 // function that holds player info;
 
 
-
-/* })();
- */
 
